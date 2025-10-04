82ff4651add925bda8578a9f86b2ba653bc1b67c
Huy Duong <huy.duong@ologn.tech>
2025-10-04 23:16:17 +0700

    Fix RV1106 build

--- rkaiq_tool_server/scripts/buildScripts/make-1106.bash
+++ rkaiq_tool_server/scripts/buildScripts/make-1106.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cmake -D CMAKE_C_COMPILER=${PWD}/../../../../tools/linux/toolchain/arm-rockchip830-linux-uclibcgnueabihf/bin/arm-rockchip830-linux-uclibcgnueabihf-gcc \
--D CMAKE_CXX_COMPILER=${PWD}/../../../../tools/linux/toolchain/arm-rockchip830-linux-uclibcgnueabihf/bin/arm-rockchip830-linux-uclibcgnueabihf-g++ \
-CMakeLists.txt
+cmake -D CMAKE_C_COMPILER=$RV1106_IPC_SDK_DIR/tools/linux/toolchain/arm-rockchip830-linux-uclibcgnueabihf/bin/arm-rockchip830-linux-uclibcgnueabihf-gcc \
+      -D CMAKE_CXX_COMPILER=$RV1106_IPC_SDK_DIR/tools/linux/toolchain/arm-rockchip830-linux-uclibcgnueabihf/bin/arm-rockchip830-linux-uclibcgnueabihf-g++ \
+      CMakeLists.txt
 
